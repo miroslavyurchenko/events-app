42ee64a3084f55f1e989d04bd28691f8259da0ea
M1roslav <117571837+miroslavyurchenko@users.noreply.github.com>
2026-05-05 23:55:24 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -306,4 +306,4 @@ app.get("/admin/events/:id/registrations", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
